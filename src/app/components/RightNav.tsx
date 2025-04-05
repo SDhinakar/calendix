50967594c0ba881ea6bdcc27285627bff23ed333
Dhinakar S <dhinakarshanmugam6@gmail.com>
2025-04-05 12:26:57 +0530

    changed

--- src/app/components/RightNav.tsx
+++ src/app/components/RightNav.tsx
@@ -16,7 +16,7 @@ export default function RightNav({email}:{email:string}) {
   else {
     return (
       <nav className="flex items-center gap-4">
-        {/* <Link href={'/api/auth'}>Sign in</Link> */}
+        <Link href={'/api/auth'}>Sign in</Link>
         <Link href={'/api/auth'} className="bg-blue-600 text-white py-2 px-4 rounded-full">Get started</Link>
       </nav>
     );
